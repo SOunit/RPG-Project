04fe99d15dc0170fa739ee8ad8aa586fd17ef548
SOUnit <workbookwork7@gmail.com>
2022-11-12 11:49:47 -0800

    add cancel combat logic

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -14,11 +14,12 @@ namespace RPG.Combat
 
         private void Update()
         {
-            bool isInRange =
-                Vector3.Distance(transform.position, target.position) <
-                weaponRange;
+            if (target == null)
+            {
+                return;
+            }
 
-            if (target != null && !isInRange)
+            if (!GetIsInRange())
             {
                 GetComponent<Mover>().MoveTo(target.position);
             }
@@ -28,10 +29,21 @@ namespace RPG.Combat
             }
         }
 
+        private bool GetIsInRange()
+        {
+            return Vector3.Distance(transform.position, target.position) <
+            weaponRange;
+        }
+
         public void Attack(CombatTarget combatTarget)
         {
             Debug.Log("Attack!!!");
             target = combatTarget.transform;
+        }
+
+        public void Cancel()
+        {
+            target = null;
         }
     }
 }
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -23,6 +23,12 @@ namespace RPG.Movement
             UpdateAnimator();
         }
 
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<Fighter>().Cancel();
+            MoveTo (destination);
+        }
+
         public void MoveTo(Vector3 destination)
         {
             navMeshAgent.destination = destination;
