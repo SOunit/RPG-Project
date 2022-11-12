ce0457b0ac51b19aaf1bf48df41b1f7739eaf6e4
SOUnit <workbookwork7@gmail.com>
2022-11-11 19:37:18 -0800

    add visible ray to debug

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -8,8 +8,16 @@ public class Mover : MonoBehaviour
     [SerializeField]
     Transform target;
 
+    Ray lastRay;
+
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+
         GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
