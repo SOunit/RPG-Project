d70b766552c4edd57c5ea3375b5eb60ebc8f5993
SOUnit <workbookwork7@gmail.com>
2022-11-12 00:48:06 -0800

    add player-controller

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit)
+        {
+            GetComponent<Mover>().MoveTo(hit.point);
+        }
+    }
+}
