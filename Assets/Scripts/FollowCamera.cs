978481c9f472b530160b9abe30725b9a5b018ea9
SOUnit <workbookwork7@gmail.com>
2022-11-12 00:14:16 -0800

    update follow camera timing to late-update

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -7,7 +7,7 @@ public class FollowCamera : MonoBehaviour
     [SerializeField]
     Transform target;
 
-    void Update()
+    void LateUpdate()
     {
         transform.position = target.position;
     }
