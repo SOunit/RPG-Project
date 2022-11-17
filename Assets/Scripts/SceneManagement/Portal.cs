63d7484afe5321839ec1440b947abf5be1b415ed
SOUnit <workbookwork7@gmail.com>
2022-11-16 20:28:52 -0800

    spawn player in spawn point

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using RPG.Control;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagement
@@ -10,12 +13,54 @@ namespace RPG.SceneManagement
         [SerializeField]
         int sceneToLoad = -1;
 
+        [SerializeField]
+        Transform spawnPoint;
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Player")
             {
-                SceneManager.LoadScene (sceneToLoad);
+                StartCoroutine(Transition());
             }
+        }
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad (gameObject);
+            SceneManager.LoadScene (sceneToLoad);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer (otherPortal);
+
+            // run in next scene
+            print("Scene Loaded");
+            Destroy (gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player
+                .GetComponent<NavMeshAgent>()
+                .Warp(otherPortal.spawnPoint.position);
+
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (var portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this)
+                {
+                    continue;
+                }
+
+                return portal;
+            }
+
+            return null;
         }
     }
 }
