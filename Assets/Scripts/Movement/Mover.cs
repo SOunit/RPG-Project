b373a18b02292851784fd5bfd22ee86251ca87f9
SOUnit <workbookwork7@gmail.com>
2022-11-17 18:40:02 -0800

    add save / load logic

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -1,10 +1,11 @@
 using RPG.Core;
+using RPG.Saving;
 using UnityEngine;
 using UnityEngine.AI;
 
 namespace RPG.Movement
 {
-    public class Mover : MonoBehaviour, IAction
+    public class Mover : MonoBehaviour, IAction, ISaveable
     {
         [SerializeField]
         Transform target;
@@ -57,6 +58,24 @@ namespace RPG.Movement
 
             float speed = localVelocity.z;
             GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+
+        public object CaptureState()
+        {
+            return new SerializableVector3(transform.position);
+        }
+
+        public void RestoreState(object state)
+        {
+            SerializableVector3 position = (SerializableVector3) state;
+
+            // to avoid conflict with nav-mesh-agent
+            GetComponent<NavMeshAgent>().enabled = false;
+
+            transform.position = position.ToVector();
+
+            // enable nav-mesh-agent after setup position
+            GetComponent<NavMeshAgent>().enabled = true;
         }
     }
 }
