f9aede7bacd4fac5fc39d11afc12387a186eb1f3
SOUnit <workbookwork7@gmail.com>
2022-11-19 23:33:49 -0800

    add arrow projectile

---
+++ Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    Transform target = null;
+
+    [SerializeField]
+    float speed = 1f;
+
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        transform.LookAt(GetAimLocation());
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private Vector3 GetAimLocation()
+    {
+        CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
+        if (targetCapsule == null)
+        {
+            return target.position;
+        }
+
+        return target.position + Vector3.up * targetCapsule.height / 2;
+    }
+}
