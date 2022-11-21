d4f654913602ced21ae0afaaeb60a2c367297987
SOUnit <workbookwork7@gmail.com>
2022-11-20 20:01:06 -0800

    restore weapon from save data

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,11 +1,12 @@
 using System;
 using RPG.Core;
 using RPG.Movement;
+using RPG.Saving;
 using UnityEngine;
 
 namespace RPG.Combat
 {
-    public class Fighter : MonoBehaviour, IAction
+    public class Fighter : MonoBehaviour, IAction, ISaveable
     {
         [SerializeField]
         float timeBetweenAttacks = 1f;
@@ -19,9 +20,6 @@ namespace RPG.Combat
         [SerializeField]
         Weapon defaultWeapon = null;
 
-        [SerializeField]
-        string defaultWeaponName = "Unarmed";
-
         Health target;
 
         float timeSinceLastAttack = Mathf.Infinity;
@@ -30,8 +28,10 @@ namespace RPG.Combat
 
         private void Start()
         {
-            Weapon weapon = Resources.Load<Weapon>(defaultWeaponName);
-            EquipWeapon (weapon);
+            if (currentWeapon == null)
+            {
+                EquipWeapon (defaultWeapon);
+            }
         }
 
         private void Update()
@@ -145,6 +145,18 @@ namespace RPG.Combat
         {
             GetComponent<Animator>().ResetTrigger("attack");
             GetComponent<Animator>().SetTrigger("stopAttack");
+        }
+
+        public object CaptureState()
+        {
+            return currentWeapon.name;
+        }
+
+        public void RestoreState(object state)
+        {
+            string weaponName = (string) state;
+            Weapon weapon = Resources.Load<Weapon>(weaponName);
+            EquipWeapon (weapon);
         }
     }
 }
