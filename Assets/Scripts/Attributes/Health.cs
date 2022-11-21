4eb093ebebd2c4fcbe0e473f4934ce512e45cf08
SOUnit <workbookwork7@gmail.com>
2022-11-21 12:34:46 -0800

    show player health

--- Assets/Scripts/Attributes/Health.cs
+++ Assets/Scripts/Attributes/Health.cs
@@ -31,6 +31,11 @@ namespace RPG.Attributes
             }
         }
 
+        public float GetPercentage()
+        {
+            return 100 * (healthPoints / GetComponent<BaseStats>().GetHealth());
+        }
+
         private void Die()
         {
             if (isDead)
