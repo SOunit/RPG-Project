a58227c937287c689a858c069a3cc68a34e928f5
SOUnit <workbookwork7@gmail.com>
2022-11-22 13:18:58 -0800

    fix level initialization

--- Assets/Scripts/SceneManagement/SavingWrapper.cs
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -13,7 +13,12 @@ namespace RPG.SceneManagement
         [SerializeField]
         float fadeInTime = 0.2f;
 
-        IEnumerator Start()
+        private void Awake()
+        {
+            StartCoroutine(LoadLastScene());
+        }
+
+        IEnumerator LoadLastScene()
         {
             Fader fader = FindObjectOfType<Fader>();
             fader.FadeOutImmediate();
