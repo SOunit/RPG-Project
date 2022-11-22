72273b0c8a60bd7737c4de7adf2fdf8012acca42
SOUnit <workbookwork7@gmail.com>
2022-11-21 21:28:43 -0800

    add creating lookup-table logic

--- Assets/Scripts/Stats/Progression.cs
+++ Assets/Scripts/Stats/Progression.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace RPG.Stats
@@ -14,19 +15,61 @@ namespace RPG.Stats
         [SerializeField]
         ProgressionCharacterClass[] characterClasses = null;
 
+        Dictionary<CharacterClass, Dictionary<Stat, float[]>>
+            lookupTable = null;
+
         public float
         GetStat(Stat stat, CharacterClass characterClass, int level)
         {
+            BuildLookup();
+
+            // foreach (ProgressionCharacterClass
+            //     progressionClass
+            //     in
+            //     characterClasses
+            // )
+            // {
+            //     if (progressionClass.characterClass != characterClass)
+            //     {
+            //         continue;
+            //     }
+            //     foreach (ProgressionStat
+            //         progressionStat
+            //         in
+            //         progressionClass.stats
+            //     )
+            //     {
+            //         if (progressionStat.stat != stat)
+            //         {
+            //             continue;
+            //         }
+            //         if (progressionStat.levels.Length < level)
+            //         {
+            //             continue;
+            //         }
+            //         return progressionStat.levels[level - 1];
+            //     }
+            // }
+            return 0;
+        }
+
+        private void BuildLookup()
+        {
+            if (lookupTable != null)
+            {
+                return;
+            }
+
+            lookupTable =
+                new Dictionary<CharacterClass, Dictionary<Stat, float[]>>();
+
             foreach (ProgressionCharacterClass
                 progressionClass
                 in
                 characterClasses
             )
             {
-                if (progressionClass.characterClass != characterClass)
-                {
-                    continue;
-                }
+                var statLookupTable = new Dictionary<Stat, float[]>();
 
                 foreach (ProgressionStat
                     progressionStat
@@ -34,21 +77,12 @@ namespace RPG.Stats
                     progressionClass.stats
                 )
                 {
-                    if (progressionStat.stat != stat)
-                    {
-                        continue;
-                    }
-
-                    if (progressionStat.levels.Length < level)
-                    {
-                        continue;
-                    }
-
-                    return progressionStat.levels[level - 1];
+                    statLookupTable[progressionStat.stat] =
+                        progressionStat.levels;
                 }
-            }
 
-            return 0;
+                lookupTable[progressionClass.characterClass] = statLookupTable;
+            }
         }
 
         [Serializable]
